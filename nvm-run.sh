a917beb9b65e676b3310b8a93653bc2ffe690506
francisyzy <francisyzy@me.com>
2025-08-04 20:45:45 +0800

    Add nvm script

---
+++ nvm-run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Ensure NVM is available
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# Use the specific Node version
+nvm use 20.18.0
+
+npm run start
